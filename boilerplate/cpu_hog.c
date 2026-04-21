c689ce931e23e186f109af05a6774755863dd02d
chhavi2308-s <chhavi.s2308@gmail.com>
2026-04-21 13:00:45 +0530

    Update cpu_hog.c

--- boilerplate/cpu_hog.c
+++ boilerplate/cpu_hog.c
@@ -1,8 +1,34 @@
+  GNU nano 4.8                                                                                     cpu_hog.c                                                                                               
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-int main() {
-    while(1) {
-        // CPU busy loop
+static unsigned int parse_seconds(const char *arg, unsigned int fallback)
+{
+    char *end = NULL;
+    unsigned long value = strtoul(arg, &end, 10);
+
+    if (!arg || *arg == '\0' || (end && *end != '\0') || value == 0)
+        return fallback;
+    return (unsigned int)value;
+}
+
+int main(int argc, char *argv[])
+{
+    const unsigned int duration = (argc > 1) ? parse_seconds(argv[1], 10) : 10;
+    const time_t start = time(NULL);
+    time_t last_report = start;
+    volatile unsigned long long accumulator = 0;
+
+    while ((unsigned int)(time(NULL) - start) < duration) {
+        accumulator = accumulator * 1664525ULL + 1013904223ULL;
+
+        if (time(NULL) != last_report) {
+            last_report = time(NULL);
+            printf("cpu_hog alive elapsed=%ld accumulator=%llu\n", (long)(time(NULL) - start), accumulator);
+        }
     }
+
+    printf("cpu_hog finished\n");
     return 0;
 }
